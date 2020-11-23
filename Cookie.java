83a3987c3d37340aff539837231bb1639fdfbfac
haucknh <haucknh@mail.uc.edu>
2020-11-23 23:50:32 +0000

    Initial commit

---
+++ Cookie.java
@@ -0,0 +1,44 @@
+class Cookie{
+  //instance variables
+  private int count;
+  private int bakeTemp;
+  private int bakeTime;
+  private boolean isCookieBaked;
+
+  //constructor
+  Cookie(){
+    count = 0;
+    bakeTemp = 0;
+    bakeTime = 0
+    isCookieBaked = false;
+  }
+
+  //constructor
+  Cookie(int aCount, int aBakeTemp, int aBakeTime, boolean isACookieBaked){
+    count = aCount;
+    bakeTemp = aBakeTemp;
+    bakeTime = aBakeTime;
+    isCookieBaked = isACookieBaked;
+  }
+
+  //accessors
+  boolean getIsCookieBaked(){
+    return isCookieBaked;
+  }
+  int getCount(){
+    return count;
+  }
+  void setCount(){
+    count = aCount;
+  }
+
+  //bake cookies method
+  Public void bakeCookies{
+
+
+
+  }
+
+
+
+}
